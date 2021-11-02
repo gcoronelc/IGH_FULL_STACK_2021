615c8710d5e8f68df93401a7b847954f035f5750
Gustavo Coronel <gcoronelc@gmail.com>
2021-11-02 14:12:50 -0500

    Clase 08

---
+++ Clase08_02NOV21/Clase08Demos/src/clase08demos/Clase08Demos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package clase08demos;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware
+ * @cursos gcoronelc.github.io
+ */
+public class Clase08Demos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
